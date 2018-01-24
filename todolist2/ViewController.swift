709ce0e5d5208bf12c7c4e458c31e1f1dc1bc518
Omar Ihmoda <omarihmoda@omars-macbook-pro.local>
2018-01-24 08:13:46 -0800

    Initial Commit

---
+++ todolist2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todolist2
+//
+//  Created by Omar Ihmoda on 1/24/18.
+//  Copyright © 2018 Omar Ihmoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
